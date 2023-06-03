771a5cbbb774b3c366f516788fa0acceb46b6622
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-06-03 15:40:47 +0330

    online friends added

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,11 +1,43 @@
+import { Avatar, AvatarGroup, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
 export const Rightbar = () => {
   return (
     <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }} p={2}>
-      <Box position={"fixed"}>Hello</Box>
-      
+      <Box position={"fixed"}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://material-ui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar alt="kourosh hassanzadeh" />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://material-ui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://material-ui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar alt="alireza amini" />
+
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar alt="arash gholamdokht" />
+          <Avatar alt="amirreza alasti" />
+          <Avatar alt="hesam ghasemi" />
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 };
